6dbfa76518a0aaedc6246ac487283585d54f82f2
Evans Ibok <evansibok@gmail.com>
2026-03-26 09:24:53 +0100

    feat: add homebrew formula template for nanny-run/homebrew-tap

---
+++ homebrew/nannyd.rb
@@ -0,0 +1,44 @@
+# Homebrew formula for the Nanny CLI.
+#
+# This file lives at homebrew/nannyd.rb in the nanny repo for source tracking.
+# For the tap to work, copy it to the Formula/ directory of the
+# nanny-run/homebrew-tap GitHub repository:
+#
+#   https://github.com/nanny-run/homebrew-tap/blob/main/Formula/nannyd.rb
+#
+# After tagging v0.1.0, update the sha256 values using:
+#   curl -fsSL <url> | shasum -a 256
+
+class Nannyd < Formula
+  desc "Execution boundary for autonomous AI agents"
+  homepage "https://nanny.run"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/nanny-run/nanny/releases/download/v#{version}/nanny-macos-arm64.tar.gz"
+      sha256 "PLACEHOLDER_REPLACE_AFTER_RELEASE"
+    end
+
+    on_intel do
+      url "https://github.com/nanny-run/nanny/releases/download/v#{version}/nanny-macos-x86_64.tar.gz"
+      sha256 "PLACEHOLDER_REPLACE_AFTER_RELEASE"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/nanny-run/nanny/releases/download/v#{version}/nanny-linux-x86_64.tar.gz"
+      sha256 "PLACEHOLDER_REPLACE_AFTER_RELEASE"
+    end
+  end
+
+  def install
+    bin.install "nanny"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nanny --version")
+  end
+end
