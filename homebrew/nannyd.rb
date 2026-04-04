9ab29af2966718e5fc64c663d1da60aac10fe962
Evans Ibok <evansibok@gmail.com>
2026-04-04 14:41:08 +0100

    chore: bump homebrew template to 0.1.3; add complete pre-tag release checklist to CONTRIBUTING.md

--- homebrew/nannyd.rb
+++ homebrew/nannyd.rb
@@ -13,7 +13,7 @@
 class Nannyd < Formula
   desc "Execution boundary for autonomous AI agents"
   homepage "https://nanny.run"
-  version "0.1.2"
+  version "0.1.3"
   license "Apache-2.0"
 
   on_macos do
